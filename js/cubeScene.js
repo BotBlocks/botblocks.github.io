89bd817741ddbd3a4ceabc106588c9d51c6f2d91
coreykruger <coreywkruger@gmail.com>
2013-07-30 18:46:16 -0700

    Reverted Cube to original configuration

--- js/cubeScene.js
+++ js/cubeScene.js
@@ -4,16 +4,10 @@ function init( webglFlag ) {
 	var camera = new THREE.PerspectiveCamera(75, 250/220, 0.1, 500);
 	scene.add( camera );
 	
-	var geo0 = new THREE.CubeGeometry( 1.5, 1.5, 1.5 );
-	var geo1 = new THREE.CubeGeometry( 1.0, 1.0, 1.0 );
-	var geo2 = new THREE.CubeGeometry( 0.5, 0.5, 0.5 );
+	var geo0 = new THREE.CubeGeometry( 12, 12, 12 );
 	var mat = new THREE.MeshBasicMaterial( { wireframe: true } );
 	var cube0 = new THREE.Mesh( geo0, mat );
-	var cube1 = new THREE.Mesh( geo1, mat );
-	var cube2 = new THREE.Mesh( geo2, mat );
-	cube0.add(cube1);
-	cube1.add(cube2);
-	cube0.position.z = -2;
+	cube0.position.z = -16;
 	scene.add(cube0);
 
 	//  RENDERER
@@ -30,7 +24,7 @@ function init( webglFlag ) {
 	
 	var x = 0;
 	var y = 0;
-	var scale = 0.003;
+	var scale = 0.005;
 	window.addEventListener("click", function (event) {
 		x = event.clientX * scale;
 		y = event.clientY * scale;
@@ -44,12 +38,8 @@ function init( webglFlag ) {
 	function render() {
 		
 		cube0.rotation.x = - y + Math.PI / 2;
-		cube0.rotation.y = x;
-		cube1.rotation.x = - y;
-		cube1.rotation.y = x;
-		cube2.rotation.x = - y;
-		cube2.rotation.y = x;
-		
+		cube0.rotation.y = - x;
+
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
 	}
